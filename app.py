a366896fd2436c917a81e2e0822ab610757a6631
alizameller <alizameller@gmail.com>
2024-04-04 17:31:29 -0400

    updates to colors of bars and background

--- app.py
+++ app.py
@@ -4,11 +4,14 @@ app = Flask(__name__)
 
 # Placeholder data for demonstration
 tasks = [
-    {"id": 1, "name": "Homework", "details": "Details of Task 1", "start_time": "2024-04-01T15:40", "end_time": "2024-04-01T17:00", "priority": "High"},
-    {"id": 2, "name": "Presentation", "details": "Details of Task 2", "start_time": "2024-04-02T12:40", "end_time": "2024-04-02T15:00", "priority": "Medium"},
-    {"id": 3, "name": "Meeting", "details": "Details of Task 2", "start_time": "2024-04-02T12:40", "end_time": "2024-04-02T15:00", "priority": "Medium"},
-    {"id": 4, "name": "Assignment", "details": "Details of Task 2", "start_time": "2024-04-03T12:40", "end_time": "2024-04-03T15:00", "priority": "Medium"},
+    {"id": 1, "activity": "UI/UX", "name": "Homework", "details": "Details of Task 1", "start_time": "2024-04-01T15:40", "end_time": "2024-04-01T17:00", "priority": "High"},
+    {"id": 2, "activity": "Databases", "name": "Presentation", "details": "Details of Task 2", "start_time": "2024-04-02T12:40", "end_time": "2024-04-02T15:00", "priority": "Medium"},
+    {"id": 3, "activity": "Senior Projects", "name": "Meeting", "details": "Details of Task 2", "start_time": "2024-04-02T12:40", "end_time": "2024-04-02T15:00", "priority": "Medium"},
+    {"id": 4, "activity": "Databases", "name": "Assignment", "details": "Details of Task 2", "start_time": "2024-04-03T12:40", "end_time": "2024-04-03T15:00", "priority": "Medium"},
+    {"id": 5, "activity": "UI/UX", "name": "Assignment", "details": "Details of Task 2", "start_time": "2024-04-03T12:40", "end_time": "2024-04-03T15:00", "priority": "Medium"},
 ]
+
+colors = {"Databases": "rgb(226, 0, 246)", "UI/UX": "rgb(73, 246, 250)"}
 
 @app.route('/')
 def index():
@@ -30,7 +33,7 @@ def signup():
 
 @app.route('/dashboard')
 def dashboard():
-    return render_template('dashboard.html', tasks=tasks)
+    return render_template('dashboard.html', tasks=tasks, colors=colors)
 
 @app.route('/monthly_calendar')
 def monthly_calendar():
