9ef038d51a4353d8402c1a84179c282271b44d3a
Aliza Meller <49292194+alizameller@users.noreply.github.com>
2024-05-03 18:53:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify2
+from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import pyotp
